551fd423e9c69e316b894c3e8a144f0623e4a984
Unknown <loic.dessaules@gmail.com>
2018-02-13 15:30:06 +0100

    Added function to fill country with a color choose by criticality

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+
 var width = 760,
     height = 700;
 
@@ -47,5 +48,45 @@ function createMap(width, height){
                 d3.select(this)
                     .attr("fill", "#A9A9A9");
             });
+
+        // We will fill all country area with a color will define the criticity
+        fillColorByCriticality();
     });
+
+}
+
+function fillColorByCriticality(){
+    let csvCo2EmissionFile = "datas/co2-emission2.csv"
+    let colors = ["#628A48", "#AFA73D", "#F9CB01", "#FAA526", "#CB3430"]
+
+    d3.dsv(";")(csvCo2EmissionFile, function(error, data){
+        data.forEach( function(element, index) {
+            if(element[2014] < 2.77){
+                $(".area[data-iso3="+element["Country Code"]+"]").css("fill", colors[0]);
+            }
+            else if(element[2014] < 6.5){
+                $(".area[data-iso3="+element["Country Code"]+"]").css("fill", colors[1]);
+            }
+            else if(element[2014] < 10.31){
+                $(".area[data-iso3="+element["Country Code"]+"]").css("fill", colors[2]);
+            }
+            else if(element[2014] <= 17.36){
+                $(".area[data-iso3="+element["Country Code"]+"]").css("fill", colors[3]);
+            }
+            else if(element[2014] > 17.36){
+                $(".area[data-iso3="+element["Country Code"]+"]").css("fill", colors[4]);
+            }
+        });
+
+    });
+
+    // Hover -> change color to show where we are on
+    var lastHoveredColor;
+    $(".area").hover(function(){
+        lastHoveredColor = $(this).css("fill");
+        $(this).css("fill", "#0f0");
+    }, function(){
+        $(this).css("fill", lastHoveredColor);
+    });
+
 }
